096c7438b31206defc8c298b575e14c9f3487187
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-04-22 16:12:04 +0700

    add display

--- BangunDatar/BangunDatar.cpp
+++ BangunDatar/BangunDatar.cpp
@@ -20,4 +20,9 @@ class bangunDatar {
             return panjang * Lebar;
         }
 
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << Lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
 };
